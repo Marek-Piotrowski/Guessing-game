f62b5d746927536b61dc4a1f55333cdfd52035ab
Marek <crue1@wp.pl>
2022-05-20 13:10:35 +0200

    Ajax buttons implemented.

---
+++ Guessing Game/Controllers/AjaxController.cs
@@ -0,0 +1,74 @@
+﻿using Guessing_Game.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Guessing_Game.Controllers
+{
+    
+    public class AjaxController : Controller
+    {
+        [Route("/Ajax")]
+        public IActionResult Index()
+        {
+            
+
+            return View();
+        }
+
+        [Route("/Ajax/People")]
+        public IActionResult People()
+        {
+            PeopleViewModel model = new PeopleViewModel();
+
+            return PartialView("_PersonList",model.people);
+        }
+
+        [HttpPost]
+        [Route("/Ajax/{personID?}")]
+        public IActionResult Index(string personID)
+        {
+            //PeopleViewModel model = new PeopleViewModel();
+
+            int number = int.Parse(personID);
+
+            
+
+            if (PeopleList._list.Find(x => x.Id == number) != null)
+            {
+                Person queryperson = PeopleList._list.Find(x => x.Id == number);
+
+                return PartialView("_PersonItem",queryperson);
+            }
+
+
+            return PartialView("_IdNotFound");
+        }
+
+        [HttpPost]
+        [Route("/Ajax/Delete/{personID?}")]
+        public IActionResult Delete(string personID)
+        {
+            
+
+            int number = int.Parse(personID);
+
+
+
+            if (PeopleList._list.Find(x => x.Id == number) != null)
+            {
+                Person queryperson = PeopleList._list.Find(x => x.Id == number);
+
+                PeopleList._list.Remove(queryperson);
+
+                return PartialView("_onSuccessDelete", queryperson);
+            }
+
+
+            return PartialView("_DeleteMessage");
+        }
+
+
+
+    }
+}
---
+++ Guessing Game/wwwroot/main.js
@@ -0,0 +1,107 @@
+﻿const container = $(".toOverride");
+const buttonPeople = $("#buttonPeople");
+const buttonDetails = $("#buttonDetails");
+const buttonDelete = $("#buttonDelete");
+
+const IdInput = $("#personID");
+const deleteInput = $("#DeletePersonID");
+
+
+
+const URL1 = "http://localhost:25586/Ajax/People"
+const URL2 = "http://localhost:25586/Ajax"
+const URL3 = "http://localhost:25586/Ajax/Delete"
+
+
+buttonPeople.on("click", function () {
+    
+    $.ajax({
+        url: URL1,
+        type: "GET",
+        contentType: "application/json",
+        success: function (data) {
+            console.log(data);
+            container.empty();
+            container.append(data)
+        },
+        error: function (error) {
+            console.log(error);
+        },
+        complete: function () {
+            console.log("Complete fetching");
+        },
+    })
+
+    
+});
+
+buttonDetails.on("click", function (e) {
+
+    let inputValue = IdInput.val();
+    
+   
+    // if value is empty set it to random 1404 number
+    if (inputValue === "") {
+
+        inputValue = 1404;
+    }
+
+
+    $.ajax({
+        url: URL2 + "/" + inputValue,
+        type: "POST",
+        contentType: "application/json",
+        success: function (data) {
+            console.log(data);
+            container.empty();
+            container.append(data)
+        },
+        error: function (error) {
+            console.log(error);
+        },
+        complete: function () {
+            console.log("Complete fetching");
+        },
+    })
+});
+
+
+
+buttonDelete.on("click", function () {
+
+    let inputValue = deleteInput.val();
+
+    // if value is empty set it to random 1404 number
+    if (inputValue === "") {
+
+        inputValue = 1404;
+    }
+
+
+    $.ajax({
+        url: URL3 + "/" + inputValue,
+        type: "POST",
+        contentType: "application/json",
+        success: function (data,textStatus, xhr) {
+            
+            container.empty();
+            container.append("<br/>");
+
+            container.append("<p> Status code  </p> " + xhr.status + " " + xhr.statusText)
+            
+            container.append(data)
+        },
+        error: function (jqXHR, error) {
+            
+            container.empty();
+            container.append("<br/>");
+            container.append("<p> Status code </p> " + jqXHR.status + " " + jqXHR.statusText)
+        },
+        complete: function () {
+            console.log("Complete fetching");
+        },
+    })
+});
+
+
+
